33b963b8144540280771079145c7d57d0de6df85
andreyVetlugin <32760728+andreyVetlugin@users.noreply.github.com>
2020-11-11 10:08:53 +0500

    add logs class in git

---
+++ MonoGame1/Logs/GameInfoLog.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoGame1.Logs
+{
+    public class GameInfoLog
+    {
+        public Point SpawnCoordinate { get; private set; }
+        public List<Point> LootBoxCoordinates { get; private set; }
+        public List<Point> Path { get; private set; }
+
+        public GameInfoLog(List<Point> path, List<Point> lootBoxCoordinates, Point playerCoordinate)
+        {
+            Path = path;
+            SpawnCoordinate = playerCoordinate;
+            LootBoxCoordinates = lootBoxCoordinates;
+        }
+    }
+}
